73ddc320ebaa6bb99a7abeed928988080959f287
Tanujrawat <61357444+Tanujrawat@users.noreply.github.com>
2021-04-13 15:04:44 +0530

    Update MyBinarySearchTree.java

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -5,44 +5,86 @@ import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     //complete this class
+    
+    private Node<E> root;
 
     @Override
-    public void insert(E data) {
+    public void insert(E data)
+	{
+		root = insert(root, data);
+	}
 
-    }
+	private Node<E> insert(Node<E> root, E data)
+	{
+		
+		if (root == null)
+		{
+			return new Node<E>(data);
+		}
+		
+		else if (data.compareTo(root.data) < 0)
+		{
+			root.left = insert(root.left, data);
+		}
+		
+		else 
+		{
+			root.right = insert(root.right, data);
+		}
+		
+
+		return root;
+	}
+    
+    @Override
+    public boolean search(E data)
+	{
+		return search(root, data);
+	}
+
+	private boolean search(Node<E> root, E data)
+	{
+		
+		if (root == null)
+		{
+			return false;
+		}
+		
+		else if (data.compareTo(root.data) < 0)
+		{
+			return contains(root.left, data);
+		}
+		
+		else if (data.compareTo(root.data) > 0)
+		{
+			return contains(root.right, data);
+		}
+		
+		else
+		{
+			return true;
+		}
+	}
+    
 
     @Override
-    public boolean search(E searchElement) {
-        return false;
-    }
+    public void inorder()
+	{
+		System.out.print("In-order Traversal:");
+		inorder(root);
+		System.out.println();
+	}
 
-    @Override
-    public void inOrder(Node<E> node) {
+	
+	private void inorder(Node<E> root)
+	{
+		if (root == null)
+			return;
 
-    }
+		inorder(root.left);
+		System.out.print(" " + root.data);
+		inorder(root.right);
+	}
 
-    @Override
-    public void preOrder(Node<E> node) {
-
-    }
-
-    @Override
-    public void postOrder(Node<E> node) {
-
-    }
-
-    @Override
-    public void reverseInOrder(Node<E> node) {
-
-    }
-
-    @Override
-    public void delete(E data) {
-
-    }
-
-    @Override
-    public int height(Node<E> node) {
-        return 0;
-    }
+   
 }
